050ca845b2f85d6d1f733beea86f5381215857f0
Anthony-Paul Fontana <anthony.paulf1@gmail.com>
2022-02-05 20:27:27 -0500

    finished homepage layout - need to add pic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,19 +4,20 @@
       <div class="col-lg-8 col-md-12">
         <div class="">
           <h1 class="home-h1">Hey, I'm Anthony-Paul</h1>
-          <div class="home-body-text-wrapper">
-            <h2 class="home-body-text">
-              I am a recent computer engineering graduate with a passion for
-              coding
-            </h2>
-          </div>
+
+          <h2 class="home-body-text">
+            I am a recent computer engineering graduate with a passion for
+            coding
+          </h2>
 
           <router-link class="btn-projects" to="/Projects"
             >My Projects</router-link
           >
         </div>
       </div>
-      <div class="col-lg-4 col-md-12 p-5"></div>
+      <div class="col-lg-4 col-md-12 pic-wrapper">
+        <div class="pic-ofme"></div>
+      </div>
     </div>
   </div>
 </template>
