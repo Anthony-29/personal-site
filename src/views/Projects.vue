4db4c62a842bee58ddad0460643f6db2d7fda6f4
Anthony-Paul Fontana <anthony.paulf1@gmail.com>
2022-02-05 21:21:59 -0500

    added cards in projects page

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,13 +1,18 @@
 <template>
-  <h1>Title</h1>
+  <h1 class="projects-h1">Projects</h1>
+  <ProjectCards />
 </template>
 
 <script>
-export default {
+import ProjectCards from "../components/ProjectCards.vue";
 
-}
+export default {
+  name: "Porjects",
+  components: {
+    ProjectCards,
+  },
+};
 </script>
 
 <style>
-
 </style>
