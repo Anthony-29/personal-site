b555cb9f14a8b7a66a9e773a42abc067fd401291
Anthony-Paul Fontana <anthony.paulf1@gmail.com>
2022-02-07 15:33:39 -0500

    implemented emailjs - cleared fields after submit

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -111,6 +111,10 @@ export default {
             console.log("FAILED...", error);
           }
         );
+
+        this.msg = ""
+        this.email = ""
+        this.name = ""
     },
   },
 };
