460f0ca6ce2a8a69cc25fd6573eee568defc78e6
Anthony-Paul Fontana <anthony.paulf1@gmail.com>
2022-02-06 22:35:16 -0500

    Added contact form styles

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,7 +8,13 @@
         <p class="about-text">
           Hi, I'm <span class="about-bold"> Anthony-Paul Fontana</span> - a
           recent Computer Engineering Technology Honours graduate from Humber
-          College in Etobicoke, Ontario.
+          College in Etobicoke, Ontario. I wrote my first line of code when I
+          was 16 years old, and have been in love with it ever since.
+        </p>
+        <br>
+        <p class="about-text">
+          I have experience in HTML/CSS, JavaScript, Vue.JS, Python, Java, C,
+          OOP and Bootstrap. Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, quae quaerat aspernatur minus ea non rerum voluptatum perferendis doloribus illo magni nemo! Eum earum excepturi cupiditate sapiente exercitationem. Vel aliquid ipsam distinctio esse cumque modi voluptate qui laboriosam est perferendis sit quam, doloribus quaerat perspiciatis necessitatibus reiciendis officia tempora ab amet accusantium repellendus quidem voluptates illo pariatur. Voluptate quam libero voluptatibus porro facere earum voluptas architecto eveniet soluta? Optio officia eos aspernatur culpa nam eius asperiores voluptatibus suscipit excepturi, consequatur corrupti alias, ut quidem ipsa? Vel recusandae quo alias nesciunt, ipsum incidunt! Dignissimos modi mollitia hic eligendi dolore praesentium eaque?
         </p>
       </div>
       <div class="col-2"></div>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,13 +1,61 @@
 <template>
-  <h1>Title</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-3"></div>
+      <div class="col-lg-6 col-md-12">
+        <div class="container-contact">
+          <form id="contact">
+            <h3 class="contact-h3">Contact Me!</h3>
+            <fieldset>
+              <input
+                placeholder="Your name"
+                type="text"
+                tabindex="1"
+                required
+                autofocus
+              />
+            </fieldset>
+            <fieldset>
+              <input
+                placeholder="Your Email Address"
+                type="email"
+                tabindex="2"
+                required
+              />
+            </fieldset>
+            <fieldset>
+              <textarea
+                placeholder="Type your message here...."
+                tabindex="5"
+                required
+              ></textarea>
+            </fieldset>
+            <fieldset>
+              <button
+                name="submit"
+                type="submit"
+                id="contact-submit"
+                data-submit="...Sending"
+              >
+                Submit
+              </button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+      <div class="col-3"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
+<style scoped>
+.container-contact,
+.contact-h3,
+fieldset {
+  background-color: white;
+}
 </style>
