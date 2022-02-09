1f92c7a610c4d5e9773af0361fc9b38e31cc28ad
Anthony-Paul Fontana <anthony.paulf1@gmail.com>
2022-02-08 19:46:42 -0500

    Foxed transition durations of image and fixed sizing issue of avatar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,18 +2,15 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-8 col-md-12">
-        <div class="">
-          <h1 class="home-h1">Hey, I'm Anthony-Paul</h1>
+        <h1 class="home-h1">Hey, I'm Anthony-Paul</h1>
 
-          <h2 class="home-body-text">
-            I am a recent computer engineering graduate with a passion for
-            coding
-          </h2>
+        <h2 class="home-body-text">
+          I am a recent computer engineering graduate with a passion for coding
+        </h2>
 
-          <router-link class="btn-projects" to="/Projects"
-            >My Projects</router-link
-          >
-        </div>
+        <router-link class="btn-projects" to="/Projects"
+          >My Projects</router-link
+        >
       </div>
       <div class="col-lg-4 col-md-12">
         <div class="pic-wrapper">
