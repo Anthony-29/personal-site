7d185ff1761f376b79b85aa5ac62f36500caa1a3
Anthony-Paul Fontana <anthony.paulf1@gmail.com>
2022-02-09 22:37:35 -0500

    finished about me info

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,17 +4,36 @@
       <div class="col-2"></div>
       <div class="col-lg-8 col-md-12">
         <h1 class="about-h1">About Me</h1>
-
         <p class="about-text">
           Hi, I'm <span class="about-bold"> Anthony-Paul Fontana</span> - a
           recent Computer Engineering Technology Honours graduate from Humber
-          College in Etobicoke, Ontario. I wrote my first line of code when I
-          was 16 years old, and have been in love with it ever since.
-        </p>
-        <br>
-        <p class="about-text">
-          I have experience in HTML/CSS, JavaScript, Vue.JS, Python, Java, C,
-          OOP and Bootstrap. Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, quae quaerat aspernatur minus ea non rerum voluptatum perferendis doloribus illo magni nemo! Eum earum excepturi cupiditate sapiente exercitationem. Vel aliquid ipsam distinctio esse cumque modi voluptate qui laboriosam est perferendis sit quam, doloribus quaerat perspiciatis necessitatibus reiciendis officia tempora ab amet accusantium repellendus quidem voluptates illo pariatur. Voluptate quam libero voluptatibus porro facere earum voluptas architecto eveniet soluta? Optio officia eos aspernatur culpa nam eius asperiores voluptatibus suscipit excepturi, consequatur corrupti alias, ut quidem ipsa? Vel recusandae quo alias nesciunt, ipsum incidunt! Dignissimos modi mollitia hic eligendi dolore praesentium eaque?
+          College in Etobicoke, Ontario. During my time at Humber I learned a
+          variety of skills such as, hardware and software, electronics and
+          networking. Software is what intrigued me the most and I got to learn
+          multiple languages and technologies. Some of these languages and
+          technologies include: HTML/CSS, JavaScript, Vue.js, Python, Java, C,
+          Bootstrap, MySQL, PHP and Object Oriented Programming (OOP).
+          <br /><br />
+          Two of my favourite projects I have built are a solar panel tracker
+          and a covid security system. The Solar panel tracker was coded in C
+          and utilizes a Raspberry PI 3 along with multiple hardware components.
+          The purpose of the project was to get the software and hardware to
+          work together so that the solar panel could track the sun. Next, the
+          Covid Security System was a group capstone project that had 2 parts
+          that worked together. The first part was, an Android application that
+          was coded in Java using the Android studio IDE. The application used
+          the firebase database to display information based on the specific
+          user that was logged in. The second part was the hardware, a Raspberry
+          PI, a custom PCB and 4 sensors were used for this. The sensors worked
+          together to check if the person using the device had a fever. If a
+          fever was not detected, the information would get sent to the database
+          and a motor would unlock a door. If a fever was detected the
+          information would not only get logged, but an admin on the app would
+          get notified about it and the motor would keep the door locked.
+          <br /><br />
+          A little about me personally. I enjoy sports, I played both hockey, as
+          a goalie, and football, as an offensive lineman, growing up and my
+          favourite teams are the Montreal Canadiens and the Green Bay Packers.
         </p>
       </div>
       <div class="col-2"></div>
