696b4cbf53664eb8aa6fca95d9569e3a0fc782f2
Anthony-Paul Fontana <anthony.paulf1@gmail.com>
2022-03-07 20:10:25 -0500

    Spell checked about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -15,21 +15,17 @@
           Bootstrap, MySQL, PHP and Object Oriented Programming (OOP).
           <br /><br />
           Two of my favourite projects I have built are a solar panel tracker
-          and a covid security system. The Solar panel tracker was coded in C
-          and utilizes a Raspberry PI 3 along with multiple hardware components.
-          The purpose of the project was to get the software and hardware to
-          work together so that the solar panel could track the sun. Next, the
-          Covid Security System was a group capstone project that had 2 parts
-          that worked together. The first part was, an Android application that
-          was coded in Java using the Android studio IDE. The application used
-          the firebase database to display information based on the specific
-          user that was logged in. The second part was the hardware, a Raspberry
-          PI, a custom PCB and 4 sensors were used for this. The sensors worked
-          together to check if the person using the device had a fever. If a
-          fever was not detected, the information would get sent to the database
-          and a motor would unlock a door. If a fever was detected the
-          information would not only get logged, but an admin on the app would
-          get notified about it and the motor would keep the door locked.
+          and a Covid Security System. The solar panel tracker was coded in C
+          and utilized a Raspberry PI 3 along with multiple hardware components.
+          Next, the Covid Security System was a group capstone project, which
+          had two parts that worked in unison. The first part was an Android
+          application that was coded in Java using the Android Studio IDE. The
+          application used the firebase database to display information based on
+          the specific user that was logged in. The second part was the
+          hardware, a Raspberry PI 3, a custom PCB, and 4 sensors were used for
+          this. The sensors worked together to check if the person using it was
+          safe to enter the building. All the information from the sensors got
+          uploaded to firebase to be displayed on the app.
           <br /><br />
           A little about me personally. I enjoy sports, I played both hockey, as
           a goalie, and football, as an offensive lineman, growing up and my
